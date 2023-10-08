c6f0c4092aad670a176e0f0674b8af370d902f7a
losting <moe.losting@gmail.com>
2023-10-08 12:21:34 +0800

    feat: 内置主题模式切换

--- packages/core/src/expose.ts
+++ packages/core/src/expose.ts
@@ -5,6 +5,7 @@ export * from './utils/tags';
 export * from './state/collapse';
 
 export { default as CubTopMixedMenu } from './components/cub-top-menu/cub-top-mixed-menu.vue';
-// export * from './hooks/use-tags-view';
+
+export * from './hooks/use-mode';
 
 export { CubInjectionKey } from './layout-provider';
